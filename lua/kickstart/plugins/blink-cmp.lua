493a11ee203e7371aeb8850c61cb20ed58eec8a2
watermelonabc <2172718424@qq.com>
2026-09-11 13:43:46 +0800

    Change keymap to "enter" to accept in blink-cmp

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -39,7 +39,7 @@ require('blink.cmp').setup {
     -- <c-k>: Toggle signature help
     --
     -- See `:help blink-cmp-config-keymap` for defining your own keymap
-    preset = 'default',
+    preset = 'enter',
 
     -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
     --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
