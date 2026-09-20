1bd71b91c37073ecc0f044ad160e1496797f866d
Watermelonabc <2172718424@qq.com>
2026-09-20 23:19:47 +0800

    Switch theme to Catppuccin

--- lua/kickstart/plugins/catppuccin.lua
+++ lua/kickstart/plugins/catppuccin.lua
@@ -1,22 +1,15 @@
-local function gh(repo) return 'https://github.com/' .. repo end
-
 -- [[ Colorscheme ]]
 -- You can easily change to a different colorscheme.
 -- Change the name of the colorscheme plugin below, and then
 -- change the command under that to load whatever the name of that colorscheme is.
 --
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-vim.pack.add { gh 'folke/tokyonight.nvim' }
+vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
 ---@diagnostic disable-next-line: missing-fields
-require('tokyonight').setup {
-  styles = {
-    comments = { italic = false }, -- Disable italics in comments
-  },
-}
-
+require("catppuccin").setup({
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+})
 -- Load the colorscheme here.
--- Like many other themes, this one has different styles, and you could load
--- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-vim.cmd.colorscheme 'tokyonight-night'
+vim.cmd.colorscheme 'catppuccin-nvim'
 
 -- vim: ts=2 sts=2 sw=2 et
